b60c48bc6d10a6eafb9fc95ff4762a65a8affe3d
Aleksey Zabrodin <alekseyzabrodin2.0@gmail.com>
2021-03-23 10:41:55 +0300

    +Add new project

---
+++ MySQL_conector/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace MySQL_conector
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.dgv = new System.Windows.Forms.DataGridView();
+      this.scripter = new System.Windows.Forms.RichTextBox();
+      ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(86, 373);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(130, 45);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "DB CONNECTING";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(323, 373);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(130, 45);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "REQUEST";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // button3
+      // 
+      this.button3.Location = new System.Drawing.Point(558, 373);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(130, 45);
+      this.button3.TabIndex = 2;
+      this.button3.Text = "CLEANER";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // dgv
+      // 
+      this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dgv.Location = new System.Drawing.Point(24, 12);
+      this.dgv.Name = "dgv";
+      this.dgv.Size = new System.Drawing.Size(748, 188);
+      this.dgv.TabIndex = 3;
+      // 
+      // scripter
+      // 
+      this.scripter.Location = new System.Drawing.Point(24, 242);
+      this.scripter.Name = "scripter";
+      this.scripter.Size = new System.Drawing.Size(748, 96);
+      this.scripter.TabIndex = 4;
+      this.scripter.Text = "";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.scripter);
+      this.Controls.Add(this.dgv);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.DataGridView dgv;
+    private System.Windows.Forms.RichTextBox scripter;
+  }
+}
+
---
+++ MySQL_conector/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using SD = System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MySQL_conector
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+    public MySqlConnection myconnector;
+    public MySqlCommand mycom;
+    public string constr = "Server=localhost;Database=medxregistry;Uid=root;charset=utf8;";
+    public SD.DataSet ds;
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        myconnector = new MySqlConnection(constr);
+        myconnector.Open();
+        MessageBox.Show("DB CONNECT");
+        myconnector.Close();
+      }
+      catch
+      {
+        MessageBox.Show("Хрен тебе!!!");
+      }
+    }
+    private void button2_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        string script = scripter.Text;
+        myconnector = new MySqlConnection(constr);
+        myconnector.Open();
+        MySqlDataAdapter ms_data = new MySqlDataAdapter(script, myconnector);
+        SD.DataTable table = new SD.DataTable();
+        ms_data.Fill(table);
+        dgv.DataSource = table;
+        myconnector.Close();
+      }
+      catch
+      {
+        MessageBox.Show("Connection Lost");
+      }
+    }
+    private void button3_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        myconnector = new MySqlConnection(constr);
+        myconnector.Open();
+        SD.DataTable table = new SD.DataTable();
+        dgv.DataSource = table;
+        myconnector.Close();
+      }
+      catch
+      {
+        MessageBox.Show("Error");
+      }
+    }
+  }
+}
